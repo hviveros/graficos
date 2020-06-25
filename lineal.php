76c2b32f511ad055ac3c0be846c6321280d7c203
Humberto Viveros <humberto.viverosrios@gmail.com>
2020-06-24 21:57:08 -0400

    Obtencion de datos para grafica lineal

--- lineal.php
+++ lineal.php
@@ -1,23 +1,49 @@
 <?php 
 	require_once "php/conexion.php";
+
+	$conexion = conexion();
+
+	$sql = "SELECT fechaVenta, MontoVenta FROM ventas ORDER BY fechaVenta";
+	$result = mysqli_query($conexion, $sql);
+
+	$valoresY = array(); //montos
+	$valoresX = array(); //fechas
+
+	while ($ver = mysqli_fetch_row($result)) {
+		$valoresX[] = $ver[0];
+		$valoresY[] = $ver[1];
+	}
+
+	$datosX = json_encode($valoresX);
+	$datosY = json_encode($valoresY);
 ?>
 
 <div id="graficaLineal"></div>
 
 <script>
+	//funcion para convertir un JSON a un array js
+	function crearCadenaLineal(json){
+		var parsed = JSON.parse(json);
+		var arr = [];
+		for (var x in parsed) {
+			arr.push(parsed[x]);
+		}
+		return arr;
+	}
+</script>
+
+<script>
+
+	datosX = crearCadenaLineal('<?php echo $datosX ?>');
+	datosY = crearCadenaLineal('<?php echo $datosY ?>');
+
 	var trace1 = {
-		x: [1, 2, 3, 4],
-		y: [10, 15, 13, 17],
+		x: datosX,
+		y: datosY,
 		type: 'scatter'
 	};
 
-	var trace2 = {
-		x: [1, 2, 3, 4],
-		y: [16, 5, 11, 9],
-		type: 'scatter'
-	};
-
-	var data = [trace1, trace2];
+	var data = [trace1];
 
 	Plotly.newPlot('graficaLineal', data);
 </script>
