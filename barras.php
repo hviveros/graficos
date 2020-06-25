7ffa275417708703ddeb33ae56ea979a1ddecdad
Humbert Viveros <humberto.viverosrios@gmail.com>
2020-06-24 21:22:16 -0400

    bd connection
    Creacion de la base de datos y conexion

--- barras.php
+++ barras.php
@@ -1,3 +1,7 @@
+<?php 
+	require_once "php/conexion.php";
+?>
+
 <div id="graficaBarras"></div>
 
 <script>
---
+++ php/conexion.php
@@ -0,0 +1,7 @@
+<?php 
+
+function conexion(){
+	return mysqli_connect('localhost', 'root', '', 'graficos');
+}
+
+?>
