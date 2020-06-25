b5a37573479fc5cdc1253899bb8ae876f0c13e9b
Humbert Viveros <humberto.viverosrios@gmail.com>
2020-06-24 20:49:47 -0400

    Inicio de proyecto y agregado de dependencias
    Add Bootstrap, JQuery, Plotly

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Graphics</title>
+	<link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css">
+	<script src="libs/jquery-3.5.1.min.js"></script>
+	<script src="libs/plotly-latest.min.js"></script>
+</head>
+<body>
+	
+</body>
+</html>
