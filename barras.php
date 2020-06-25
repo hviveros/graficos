6da606ade3e9999fff1e40656df2bb947d9ccbb7
Humbert Viveros <humberto.viverosrios@gmail.com>
2020-06-24 22:24:53 -0400

    Obtencion de datos para grafica de barras

--- barras.php
+++ barras.php
@@ -1,14 +1,46 @@
 <?php 
 	require_once "php/conexion.php";
+
+	$conexion = conexion();
+
+	$sql = "SELECT fechaVenta, MontoVenta FROM ventas ORDER BY fechaVenta";
+	$result = mysqli_query($conexion, $sql);
+
+	$valoresY = array(); //montos
+	$valoresX = array(); //fechas
+
+	while ($ver = mysqli_fetch_row($result)) {
+		$valoresX[] = $ver[0];
+		$valoresY[] = $ver[1];
+	}
+
+	$datosX = json_encode($valoresX);
+	$datosY = json_encode($valoresY);
 ?>
 
 <div id="graficaBarras"></div>
 
 <script>
+	//funcion para convertir un JSON a un array js
+	function crearCadenaBarras(json){
+		var parsed = JSON.parse(json);
+		var arr = [];
+		for (var x in parsed) {
+			arr.push(parsed[x]);
+		}
+		return arr;
+	}
+</script>
+
+<script>
+	
+	datosX = crearCadenaBarras('<?php echo $datosX ?>');
+	datosY = crearCadenaBarras('<?php echo $datosY ?>');
+
 	var data = [
 	{
-		x: ['giraffes', 'orangutans', 'monkeys'],
-		y: [20, 14, 23],
+		x: datosX,
+		y: datosY,
 		type: 'bar'
 	}
 	];
